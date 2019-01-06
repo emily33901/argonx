c4f6bbdab376b9188e1238c3df1855970071f51c
Emily Hudson <emily33901@gmail.com>
2019-01-06 15:05:31 +0000

    Add new buffer class

--- common/buffer.hh
+++ common/buffer.hh
@@ -204,3 +204,128 @@ public:
         return storage;
     }
 };
+
+// -----
+
+class NewBuffer {
+    std::vector<u8> storage;
+    size_t          offset = 0;
+    size_t          base   = 0;
+
+    template <typename T>
+    static constexpr bool ValidToWrite = std::is_trivially_constructible_v<T> && !std::is_pointer_v<T>;
+
+    decltype(storage.begin()) OffsetIterator() {
+        return storage.begin() + base + offset;
+    }
+
+    void WriteData(const u8 *buffer, size_t count) {
+        storage.insert(OffsetIterator(), buffer, buffer + count);
+        offset += count;
+    }
+
+    u8 *ReadDataInPlace(size_t size) {
+        auto i = &*OffsetIterator();
+        offset += size;
+
+        return i;
+    }
+
+public:
+    NewBuffer() {
+    }
+
+    void   Seek(i32 off) { offset += off; }
+    size_t Tell() { return offset; }
+
+    void   SetPos(size_t pos) { offset = pos; }
+    size_t Size() { return storage.size(); }
+
+    template <typename T>
+    void SetBase() { base = sizeof(T); }
+    void SetBase(u32 pos) { base = pos; }
+
+    template <typename T>
+    std::enable_if_t<ValidToWrite<T>> Write(const T data) {
+        const auto length = sizeof(T);
+
+        auto dataInBytes = reinterpret_cast<const u8 *>(&data);
+
+        WriteData(dataInBytes, length);
+    }
+
+    template <typename T>
+    std::enable_if_t<ValidToWrite<T>> Write(const std::pair<T *, size_t> data) {
+        auto [arr, length] = data;
+
+        auto dataInBytes   = reinterpret_cast<const u8 *>(arr);
+        auto lengthInBytes = length * sizeof(T);
+
+        WriteData(dataInBytes, lengthInBytes);
+    }
+
+    template <typename T, size_t N>
+    void Write(T (&x)[N]) {
+        Write(std::make_pair(x, N));
+    }
+
+    void Write(const NewBuffer b) {
+        return Write(b.storage);
+    }
+
+    void Write(const std::vector<u8> &b) {
+        WriteData(b.data(), b.size());
+    }
+
+    template <typename... Args>
+    std::enable_if_t<(sizeof...(Args) > 1)> Write(Args... args) {
+        (Write(std::forward<Args>(args)), ...);
+    }
+
+    template <typename T>
+    std::enable_if_t<ValidToWrite<T>, T> Read() {
+        return *reinterpret_cast<T *>(ReadDataInPlace(sizeof(T)));
+    }
+
+    template <typename T>
+    std::enable_if_t<ValidToWrite<T>, void> ReadInto(T &x) {
+        x = Read<T>();
+    }
+
+    void ReadInto(std::vector<u8> &x) {
+        assert(x.capacity() > 0); // If this goes off you havent reserved any space
+
+        x.insert(x.end(), OffsetIterator(), OffsetIterator() + x.capacity());
+    }
+
+    template <typename... Args>
+    std::enable_if_t<(sizeof...(Args) > 1)> Read(Args &... args) {
+        (ReadInto(std::forward<Args &>(args)), ...);
+    }
+};
+
+#if 0
+inline int TestNewBuffer() {
+    NewBuffer b;
+
+    std::vector<u8> v{'B', 'A'};
+
+    b.Write(13, 25, 15, 234, v);
+
+    u32 a, c, d, e;
+
+    std::vector<u8> v2;
+
+    v2.reserve(13);
+
+    b.SetPos(0);
+
+    b.Read(a, c, d, e, v2);
+
+    printf("a is %d c is %d d is %d e is %d v2 is %s\n", a, c, d, e, v2.data());
+
+    return b.Size();
+}
+
+inline int tdfssdfsdfsfdest = TestNewBuffer();
+#endif
