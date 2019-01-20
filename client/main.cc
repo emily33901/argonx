3a8fa71c145f0cc48839fc45ff0bab70d53a15b2
Emily Hudson <emily33901@gmail.com>
2019-01-20 21:01:43 +0000

    Update main

--- client/main.cc
+++ client/main.cc
@@ -9,6 +9,22 @@ int main(const int argCount, const char **argStrings) {
            Steam::InterfaceHelpers::TAllocator()->NumAllocated(),
            Steam::InterfaceHelpers::TAllocator()->BytesAllocated());
 
+    extern void *CreateInterface(const char *name, int *err);
+
+    auto a = CreateInterface("SteamUtils009", nullptr);
+    Assert(a != nullptr, "CreateInterface test failed");
+
+    extern Steam::InterfaceHelpers::InterfaceReg *GetInterfaceList();
+
+    {
+        auto head  = GetInterfaceList();
+        auto total = 0;
+
+        for (auto cur = head; cur != nullptr; cur = cur->next) {
+            total += 1;
+            printf("[I] %s\n", cur->name);
+        }
+    }
     Argonx::SteamClient sClient;
 
     printf("Pumping...\n");
